7e794810e1ae2efa65e0edbb318ddda180e74f50
darkryder <sambhav13085@iiitd.ac.in>
2015-06-28 19:07:03 +0530

    Added a quick logout button for debugging purposes.

--- app/src/main/java/com/iiitd/esya/app/MainActivity.java
+++ app/src/main/java/com/iiitd/esya/app/MainActivity.java
@@ -129,6 +129,15 @@ public class MainActivity extends ActionBarActivity
         if (id == R.id.action_settings) {
             return true;
         }
+        if (id == R.id.logout_settings){
+            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putBoolean(getString(R.string.pref_logged_in), false);
+            editor.commit();
+
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
 
         return super.onOptionsItemSelected(item);
     }
