732d7bc2d66a7f6dab96c0ab2c92a7bd74526c8f
darkryder <sambhav13085@iiitd.ac.in>
2015-07-30 20:55:32 +0530

    Event page. WIP

--- app/src/main/java/com/iiitd/esya/app/EventDetailFragment.java
+++ app/src/main/java/com/iiitd/esya/app/EventDetailFragment.java
@@ -2,10 +2,17 @@ package com.iiitd.esya.app;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.github.florent37.materialviewpager.MaterialViewPagerHelper;
+import com.github.florent37.materialviewpager.adapter.RecyclerViewMaterialAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -18,6 +25,15 @@ import android.widget.TextView;
  */
 public class EventDetailFragment extends Fragment {
 
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+
+    private static final int ITEM_COUNT = 1;
+
+    private List<Object> mContentItems = new ArrayList<>();
+
+
     public String mDescription;
 
     public static EventDetailFragment newInstance(String details)
@@ -27,12 +43,58 @@ public class EventDetailFragment extends Fragment {
         return temp;
     }
 
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        for(int i = 0; i < 250; i++)
+        mContentItems.add(new Object());
+
+
+        mAdapter = new RecyclerViewMaterialAdapter(new TestRecyclerViewAdapter(mContentItems));
+        mRecyclerView.setAdapter(mAdapter);
+
+        MaterialViewPagerHelper.registerRecyclerView(getActivity(), mRecyclerView, null);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_event_detail, container, false);
-        ((TextView)view.findViewById(R.id.text_event_details)).setText(mDescription);
-        return view;
+//        View view = inflater.inflate(R.layout.fragment_event_detail, container, false);
+//        ((TextView)view.findViewById(R.id.text_event_details)).setText(mDescription);
+//        return view;
+        return inflater.inflate(R.layout.fragment_event_detail, container, false);
+    }
+}
+
+class TestRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+{
+
+    List<Object> contents;
+
+    public TestRecyclerViewAdapter(List<Object> contents) {
+        this.contents = contents;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_card_big, parent, false);
+        return new RecyclerView.ViewHolder(view){};
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return 250;
     }
 }
