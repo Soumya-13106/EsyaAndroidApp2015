2d908866ca07895d917c5bda14311be33e5d9be2
Soumya-13106 <soumya13106@iiitd.ac.in>
2015-07-31 22:42:20 +0530

    DBHelper created. Functions for insert,delete,update,getEvent,getAllEvents created.

---
+++ app/src/main/java/com/iiitd/esya/app/DBHelper.java
@@ -0,0 +1,219 @@
+package com.iiitd.esya.app;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Soumya on 31-07-2015.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "esya.db";
+    public static final String TABLE_NAME = "event";
+    public static final String COLUMN_EVENT_ID = "event_id";
+    public static final String COLUMN_EVENT_NAME = "event_name";
+    public static final String COLUMN_CATEGORY_IDS = "category_ids";
+//    public static final String COLUMN_LAST_UPDATED = "last_updated";
+    public static final String COLUMN_IMAGE_URL = "image_url";
+    public static final String COLUMN_CONTACT = "contact";
+    public static final String COLUMN_IS_REGISTERED = "is_registered";
+    public static final String COLUMN_IS_TEAM_EVENT = "is_team_event";
+    public static final String COLUMN_TEAM_ID = "team_id";
+    public static final String COLUMN_EVENT_ELIGIBILITY = "event_eligibility";
+    public static final String COLUMN_EVENT_JUDGING = "event_judging";
+    public static final String COLUMN_EVENT_PRIZES = "event_prizes";
+    public static final String COLUMN_EVENT_RULES = "event_rules";
+    public static final String COLUMN_TEAM_SIZE = "team_size";
+    public static final String COLUMN_VENUE = "venue";
+    public static final String COLUMN_EVENT_DESCRIPTION = "event_description";
+
+    private static final String[] projection = {
+            COLUMN_EVENT_ID,
+            COLUMN_EVENT_NAME,
+            COLUMN_CATEGORY_IDS,
+//            COLUMN_LAST_UPDATED,
+            COLUMN_IMAGE_URL,
+            COLUMN_CONTACT,
+            COLUMN_IS_REGISTERED,
+            COLUMN_IS_TEAM_EVENT,
+            COLUMN_TEAM_ID,
+            COLUMN_EVENT_ELIGIBILITY,
+            COLUMN_EVENT_JUDGING,
+            COLUMN_EVENT_PRIZES,
+            COLUMN_EVENT_RULES,
+            COLUMN_TEAM_SIZE,
+            COLUMN_VENUE,
+            COLUMN_EVENT_DESCRIPTION,
+    };
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    };
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (" +
+                        COLUMN_EVENT_ID + " INT NOT NULL PRIMARY KEY, " +
+                        COLUMN_EVENT_NAME + " VARCHAR(255) NOT NULL, " +
+                        COLUMN_CATEGORY_IDS + " VARCHAR(255) NOT NULL, " +
+//                        COLUMN_LAST_UPDATED + " VARCHAR(255) NOT NULL, " +
+                        COLUMN_IMAGE_URL + " VARCHAR(255) NOT NULL, " +
+                        COLUMN_CONTACT + " VARCHAR(2048) NOT NULL, " +
+                        COLUMN_IS_REGISTERED + " INT NOT NULL DEFAULT 0, " +
+                        COLUMN_IS_TEAM_EVENT + " INT NOT NULL DEFAULT 0, " +
+                        COLUMN_TEAM_ID + " INT NOT NULL, " +
+                        COLUMN_EVENT_ELIGIBILITY + " VARCHAR(5000) NOT NULL, " +
+                        COLUMN_EVENT_JUDGING + " VARCHAR(5000) NOT NULL, " +
+                        COLUMN_EVENT_PRIZES + " VARCHAR(5000) NOT NULL, " +
+                        COLUMN_EVENT_RULES + " VARCHAR(5000) NOT NULL, " +
+                        COLUMN_TEAM_SIZE + " INT NOT NULL, " +
+                        COLUMN_VENUE + " VARCHAR(1024) NOT NULL, " +
+                        COLUMN_EVENT_DESCRIPTION + " VARCHAR(10000) NOT NULL" +
+                        ");"
+        );
+    }
+
+    private static String getCommaSeperatedCategories(Category[] category) {
+        StringBuilder result = new StringBuilder();
+        for(Category c: category) {
+            result.append(c.id).append(",");
+        }
+        return result.toString();
+    }
+
+    private static Category[] getCategoriesFromCSVString(String string) {
+        if ((string == null) || (string.trim().isEmpty())) return null;
+        String[] ids = string.split(",");
+        ArrayList<Category> categories = new ArrayList<>();
+        for(String id: ids) {
+            if (id == null || id.trim().isEmpty()) continue;
+            categories.add(Category.resolveToCategory(Integer.parseInt(id)));
+        }
+        return categories.toArray(new Category[categories.size()]);
+    }
+
+    public int returnIntFromBoolean(boolean value) {
+        if(value) {
+            return 1;
+        } return 0;
+    }
+
+    public boolean returnBooleanFromInt(int value) {
+        if(value==1) {
+            return true;
+        } return false;
+    }
+
+    public boolean insertEvent(Event event) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_EVENT_ID, event.id);
+        contentValues.put(COLUMN_EVENT_NAME, event.name);
+        contentValues.put(COLUMN_CATEGORY_IDS, getCommaSeperatedCategories(event.categories));
+//        contentValues.put(COLUMN_LAST_UPDATED, event.lastUpdated);
+        contentValues.put(COLUMN_IMAGE_URL, event.image_url);
+        contentValues.put(COLUMN_CONTACT, event.contact);
+        contentValues.put(COLUMN_IS_REGISTERED, event.registered);
+        contentValues.put(COLUMN_IS_TEAM_EVENT, event.team_event);
+        contentValues.put(COLUMN_TEAM_ID, event.team_id);
+        contentValues.put(COLUMN_EVENT_ELIGIBILITY, event.eligibility);
+        contentValues.put(COLUMN_EVENT_JUDGING, event.judging);
+        contentValues.put(COLUMN_EVENT_PRIZES, event.prizes);
+        contentValues.put(COLUMN_EVENT_RULES, event.rules);
+        contentValues.put(COLUMN_TEAM_SIZE, event.team_size);
+        contentValues.put(COLUMN_VENUE, event.venue);
+        contentValues.put(COLUMN_EVENT_DESCRIPTION, event.description);
+        long check = db.insert(TABLE_NAME, null, contentValues);
+        if(check>0) {
+            return true;
+        } return false;
+    }
+
+    public Event[] getAllEvents() {
+        ArrayList<Event> eventArrayList = new ArrayList<>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        c.moveToFirst();
+        while(c.isAfterLast() == false) {
+            eventArrayList.add(getEventFromCursor(c));
+            c.moveToNext();
+        }
+        return eventArrayList.toArray(new Event[eventArrayList.size()]);
+    }
+
+    public Event getEvent(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.query(
+                TABLE_NAME,
+                projection,
+                COLUMN_EVENT_ID + "=?",
+                new String[]{String.valueOf(id)},
+                null,
+                null,
+                null
+        );
+        if(c==null) {
+            return null;
+        }
+        c.moveToFirst();
+        return getEventFromCursor(c);
+    }
+
+    private Event getEventFromCursor(Cursor c) {
+        if(c==null) {
+            return null;
+        }
+        int eventId = c.getInt(c.getColumnIndexOrThrow(COLUMN_EVENT_ID));
+        String eventName = c.getString(c.getColumnIndexOrThrow(COLUMN_EVENT_NAME));
+        Category[] eventCategories = getCategoriesFromCSVString(c.getString(c.getColumnIndexOrThrow(COLUMN_CATEGORY_IDS)));
+//        String eventLastUpdated = c.getString(c.getColumnIndexOrThrow(COLUMN_LAST_UPDATED));
+        String eventImageUrl = c.getString(c.getColumnIndexOrThrow(COLUMN_IMAGE_URL));
+
+        Event event = new Event(eventId, eventName, eventCategories, eventImageUrl);
+
+        event.contact = c.getString(c.getColumnIndexOrThrow(COLUMN_CONTACT));
+        event.registered = returnBooleanFromInt(c.getInt(c.getColumnIndexOrThrow(COLUMN_IS_REGISTERED)));
+        event.team_event = returnBooleanFromInt(c.getInt(c.getColumnIndexOrThrow(COLUMN_IS_TEAM_EVENT)));
+        event.team_id = c.getInt(c.getColumnIndexOrThrow(COLUMN_TEAM_ID));
+        event.eligibility = c.getString(c.getColumnIndexOrThrow(COLUMN_EVENT_ELIGIBILITY));
+        event.judging = c.getString(c.getColumnIndexOrThrow(COLUMN_EVENT_JUDGING));
+        event.prizes = c.getString(c.getColumnIndexOrThrow(COLUMN_EVENT_PRIZES));
+        event.rules = c.getString(c.getColumnIndexOrThrow(COLUMN_EVENT_RULES));
+        event.team_size = c.getInt(c.getColumnIndexOrThrow(COLUMN_TEAM_SIZE));
+        event.venue = c.getString(c.getColumnIndexOrThrow(COLUMN_VENUE));
+        event.description = c.getString(c.getColumnIndexOrThrow(COLUMN_EVENT_DESCRIPTION));
+
+        return event;
+    }
+
+    public boolean updateEvent(Event event) {
+        boolean isDeleteSuccessful = deleteEvent(event.id);
+        if(!isDeleteSuccessful) {
+            return false;
+        }
+        boolean isInsertSuccessful = insertEvent(event);
+        if(isInsertSuccessful) {
+            return true;
+        } return false;
+    }
+
+    public boolean deleteEvent(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        int check = db.delete(TABLE_NAME, COLUMN_EVENT_ID + "=?", new String[] {String.valueOf(id)});
+        if(check > 0) {
+            return true;
+        } return false;
+    }
+}
